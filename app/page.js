e8d17fa66e795b71085342c779583f62a2ec3b04
sahilraut59 <sahilraut59@gmail.com>
2024-05-03 14:13:23 +0530

    hmmm

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
-
 export default function Home() {
   return (
-    <div className="border h-screen w-screen">
-      Projects Section is still in development
+    <div className="flex h-screen w-screen justify-center items-center bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 background-animate">
+      <div className="text-white text-xl md:text-2xl">
+        Projects Section is still in development
+      </div>
     </div>
   );
 }
